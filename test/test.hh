7f9da1e2823f9746b73f5ac3652b82ab05a8d247
Jouni Roivas <jroivas@iki.fi>
2017-12-21 19:42:48 +0200

    Allow multiple asserts per test case

--- test/test.hh
+++ test/test.hh
@@ -26,16 +26,15 @@ int main(int argc, char**argv) {\
     {\
         Y;\
     }\
-    std::cout << "== " << (__success - __suite_success) << " tests PASSED in suite\n";
+    std::cout << "== " << (__success - __suite_success) << " tests PASSED in suite: " << #X << "\n";
 #define TEST_CASE(X, Y) {\
     std::cout << "   TEST " << #X << "\n"; \
     Y;\
 }
-#define TEST_ONE_ASSERT bool only_one_assert_per_test_case; (void)(only_one_assert_per_test_case);
-#define TEST_ASSERT_TRUE(X) TEST_ONE_ASSERT do { if ((X)) { __success++; } else { std::stringstream s; s << #X; throw s.str(); } } while (0);
+#define TEST_ASSERT_TRUE(X) do { if ((X)) { __success++; } else { std::stringstream s; s << #X; throw s.str(); } } while (0);
 #define TEST_ASSERT_FALSE(X) TEST_ASSERT_TRUE(!X)
-#define TEST_ASSERT_EQUALS(X, Y) TEST_ONE_ASSERT do { if ((X == Y)) { __success++; } else { std::stringstream s; s << #X << " != " << #Y << ", got " << std::hex << X << std::dec; throw s.str(); } } while (0);
-#define TEST_ASSERT_EQUALS_ARRAY_INDEX(X, Y, IDX, SIZE) TEST_ONE_ASSERT do { \
+#define TEST_ASSERT_EQUALS(X, Y) do { if ((X == Y)) { __success++; } else { std::stringstream s; s << #X << " != " << #Y << ", got " << std::hex << X << std::dec; throw s.str(); } } while (0);
+#define TEST_ASSERT_EQUALS_ARRAY_INDEX(X, Y, IDX, SIZE) do { \
     for (size_t i = 0; i < SIZE; ++i) {\
         if (X[IDX + i] != Y[i]) {\
             std::stringstream s; s << "Array does not match at index " << i << " " << std::hex << X[IDX + i] << " != " << Y[i] << std::dec;\
